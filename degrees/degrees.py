72f0cccaf94c58f4eb3f971a0645cd24c386a7ba
Benj011 <bemini@purdue.edu>
2024-06-10 15:17:09 -0500

    more work

--- degrees/degrees.py
+++ degrees/degrees.py
@@ -92,6 +92,8 @@ def shortest_path(source, target):
     If no possible path, returns None.
     """
 
+    visited = {}
+
     print("ID's - ")
     print(source)
     print(target)
@@ -110,6 +112,44 @@ def shortest_path(source, target):
     # TODO
     raise NotImplementedError
 
+class Node():
+    def __init__(self, state, parent, action):
+        self.state = state
+        self.parent = parent
+        self.action = action
+
+
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+
+class QueueFrontier(StackFrontier):
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
 
 def person_id_for_name(name):
     """
