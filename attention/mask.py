991775faf5ad78c2298af9f6b2007f2d4509533f
Benj011 <bemini@purdue.edu>
2024-08-28 00:26:55 -0400

    done

--- attention/mask.py
+++ attention/mask.py
@@ -84,12 +84,23 @@ def visualize_attentions(tokens, attentions):
     (starting count from 1).
     """
     # TODO: Update this function to produce diagrams for all layers and heads.
-    generate_diagram(
-        1,
-        1,
-        tokens,
-        attentions[0][0][0]
-    )
+    # generate_diagram(
+    #     1,
+    #     1,
+    #     tokens,
+    #     attentions[0][0][0]
+    # )
+
+
+
+    for i in range(1, len(attentions)):
+        for j in range(1, len(attentions[i][0])):
+            generate_diagram(
+                i,
+                j,
+                tokens,
+                attentions[i][0][j]
+            )
 
 
 def generate_diagram(layer_number, head_number, tokens, attention_weights):
